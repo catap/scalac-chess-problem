309625391e25fbe7370f62bd12086dd9b4826925
Kirill A. Korinskiy <catap@catap.ru>
2014-12-22 11:02:59 +0700

    Rewrite ChessProble.slove to tail recursion

--- src/main/scala/ChessProblem.scala
+++ src/main/scala/ChessProblem.scala
@@ -1,16 +1,41 @@
 import Chess._
 
+import scala.annotation.tailrec
+
 object ChessProblem {
 
   def solve(board: Board, pieces: List[Piece]) = {
-    pieces.permutations.map ( pieces =>
-      (for (x <- 1 to board.x; y <- 1 to board.y) yield (x,y))
-        .permutations
-        .map(xss => xss.take(pieces.size))
-        .map(_.zip(pieces).map(p => Position(p._2, Square(p._1._1, p._1._2))) )
-        .map(occupied => Chess(board, occupied.toSet))
-        .filter(!_.isThreaten)
-    ).flatten.toSet
+
+    val all_pieces = pieces.permutations.toList
+
+    @tailrec
+    def loopTestPieces(squares: List[Square], pieces: List[List[Piece]], safeCombination: Set[Chess]): Set[Chess] = {
+      if (pieces.isEmpty) safeCombination
+      else {
+        val piece = pieces.head
+        val occupied = squares.zip(piece).map(p => Position(p._2, p._1)).toSet
+        val chess = Chess(board, occupied)
+        val newSafeCombination =
+          if (!chess.isThreaten) safeCombination ++ Set(chess)
+          else safeCombination
+        loopTestPieces(squares, pieces.drop(1), newSafeCombination)
+      }
+    }
+
+    @tailrec
+    def loopSolve(squares: List[List[(Int, Int)]], safeCombination: Set[Chess]): Set[Chess] = {
+      if (squares.isEmpty) safeCombination
+      else {
+        val square = squares.head.map(xy => Square(xy._1, xy._2))
+        val newSafeCombination = safeCombination ++ loopTestPieces(square, all_pieces, safeCombination)
+        loopSolve(squares.drop(1), newSafeCombination)
+      }
+    }
+
+    val squares = (for (x <- 1 to board.x; y <- 1 to board.y)
+    yield (x, y)).toSet.subsets(pieces.size).map(_.toList).toList
+
+    loopSolve(squares, Set())
   }
 
   def main(args: Array[String]): Unit = {
--- src/test/scala/ChessProblem$Test.scala
+++ src/test/scala/ChessProblem$Test.scala
@@ -7,21 +7,67 @@ class ChessProblem$Test extends FreeSpec {
 
       assert(solution.size == 4)
       assert(solution.mkString("\n") ==
+        "♔*♔\n" +
+          "***\n" +
+          "*♖*\n" +
+          "\n" +
+          "**♔\n" +
+          "♖**\n" +
+          "**♔\n" +
+          "\n" +
           "♔**\n" +
           "**♖\n" +
           "♔**\n" +
           "\n" +
-          "♔*♔\n" +
-          "***\n" +
-          "*♖*\n" +
-          "\n" +
           "*♖*\n" +
           "***\n" +
-          "♔*♔\n" +
+          "♔*♔\n")
+    }
+
+    "4×4 board containing 2 Rooks and 4 Knights" - {
+      val solution = ChessProblem.solve(Chess.Board(4, 4), List(Chess.Knight, Chess.Knight, Chess.Knight, Chess.Knight, Chess.Rock, Chess.Rock))
+
+      assert(solution.size == 8)
+      assert(solution.mkString("\n") ==
+          "**♖*\n" +
+          "*♘*♘\n" +
+          "♖***\n" +
+          "*♘*♘\n" +
           "\n" +
-          "**♔\n" +
-          "♖**\n" +
-          "**♔\n")
+          "♘*♘*\n" +
+          "*♖**\n" +
+          "♘*♘*\n" +
+          "***♖\n" +
+          "\n" +
+          "*♘*♘\n" +
+          "♖***\n" +
+          "*♘*♘\n" +
+          "**♖*\n" +
+          "\n" +
+          "*♘*♘\n" +
+          "**♖*\n" +
+          "*♘*♘\n" +
+          "♖***\n" +
+          "\n" +
+          "***♖\n" +
+          "♘*♘*\n" +
+          "*♖**\n" +
+          "♘*♘*\n" +
+          "\n" +
+          "*♖**\n" +
+          "♘*♘*\n" +
+          "***♖\n" +
+          "♘*♘*\n" +
+          "\n" +
+          "♘*♘*\n" +
+          "***♖\n" +
+          "♘*♘*\n" +
+          "*♖**\n" +
+          "\n" +
+          "♖***\n" +
+          "*♘*♘\n" +
+          "**♖*\n" +
+          "*♘*♘\n")
     }
   }
 }
